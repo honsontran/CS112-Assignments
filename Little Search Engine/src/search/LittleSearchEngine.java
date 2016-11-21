aea967c47b47ea7ab0122d4258ca0f428a12b9ae
Honson Tran <honsontran@gmail.com>
2016-11-21 03:25:49 -0500

    Top5 Master Push
    removed ref/remote lines

--- Little Search Engine/src/search/LittleSearchEngine.java
+++ Little Search Engine/src/search/LittleSearchEngine.java
@@ -242,13 +242,8 @@ public static void main(String[] args) throws FileNotFoundException{
 			// store all occurances within the kws in 'occurrence'
 			Occurrence occurrence = kws.get(key);
 			
-<<<<<<< HEAD
-			// if the kewords Index does not contain the key we add it to a new occurrence list and place the key value into the keywords index
-			// otherwise, insert the occurance of the keword into the appriopiate place using insertLastOccurance
-=======
 			// if the keywords Index does not contain the key we add it to a new occurrence list and place the key value into the keywords index
 			// otherwise, insert the occurrance of the keyword into the appropriate place using insertLastOccurance
->>>>>>> refs/remotes/origin/Honson
 			if(!keywordsIndex.containsKey(key))	{	
 				ArrayList<Occurrence> newOccList = new ArrayList<Occurrence>();			
 				newOccList.add(occurrence);
@@ -313,59 +308,6 @@ public static void main(String[] args) throws FileNotFoundException{
 			return true;
 		}
 		
-<<<<<<< HEAD
-		String curr = "";		//keeps track of our current keyword
-		
-		//Exception: When punctuation is in the beginning.
-		if ( Character.isLetter( word.charAt(0) ) ) {
-			return null;			
-		}
-		
-		//Turn everything to lowercase.
-		word.toLowerCase();
-		
-		//Run through the word and see if it's a letter at each char.
-		for (int i = 0; i < word.length(); i++) {
-			
-			if ( Character.isLetter( word.charAt(i) ) ) {			//If letter, add to keyword string
-				curr += word.charAt(i);
-			}
-			
-			else {	//Anything other than a letter
-				if ( Character.isDigit( word.charAt(i) ) ) {
-					return null;
-				}
-				
-				else if ( isPunctuation(word.charAt(i)) ) { //If it's punctuation
-					//Case 1: If the punctuation is between 2 chars (i.e. we're), return null.
-					if ( Character.isLetter( word.charAt(i-1) ) && Character.isLetter( word.charAt(i+1) ) ) {
-						return null;
-					}
-				
-					/* Case 2: If there's just a trailing end of punctuation (i.e. what??!!?!?)
-					 * 		   keep checking the rest of the string in the next i++
-					 */
-					
-					// Case 3: If you have punctuation and there's a letter or not a punctuation, return null.
-					if ( !isPunctuation(word.charAt(i)) || Character.isDigit( word.charAt(i+1) ) ) {
-						return null;
-					}
-					
-				}
-			}
-		}
-		
-		return curr;
-	
-	}
-	
-	private boolean isPunctuation(char c) {
-		if (c == '.' || c == ',' || c == '?' || c == ':' || c == ';' || c == '!') {
-			return true;
-		}
-		
-=======
->>>>>>> refs/remotes/origin/Honson
 		else {
 			return false;
 		}
